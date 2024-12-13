7288db6ae5b74539ee38c9a19f333376b4a73e0f
Windsor Yeh <78544120+windsoryeh@users.noreply.github.com>
2024-12-13 23:57:45 +0800

    Improved #1
    Improved version of the game.

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 function generate() {
+    clearInput()
     let beginning = Number(document.getElementById("minimum").value);
     let ending = Number(document.getElementById("maximum").value);
-    let difficulty = ending - beginning + 1;
-    
-    let answer = beginning + Math.ceil(Math.random() * difficulty) - 1;
-    
-    console.log(answer);
-    window.answer = answer
-
     let currlist = document.getElementById("list")
-    currlist.innerHTML = `<br>` + currlist.innerHTML;
+    if (ending < beginning) {
+        currlist.innerHTML = `<p class="red">Invalid range: maximum must be greater than or equal to minimum.</p><hr>` + currlist.innerHTML;
+        return;
+    }
+    else{
+        let difficulty = ending - beginning + 1;
+        
+        let answer = beginning + Math.floor(Math.random() * difficulty);
+        
+        console.log(answer);
+        window.answer = answer
+        
+        if (beginning == ending){
+            currlist.innerHTML = `<p class="green">The answer is obviously ${answer}. You are an absolute cheater.</p><hr>` + currlist.innerHTML;
+
+        }
+        else currlist.innerHTML = `<p class="green">A new number between ${beginning} and ${ending} (including) has been generated.</p><hr>` + currlist.innerHTML;
+
+    }
+    Window.relationsMax = ending
+    Window.relationsMin = beginning
+    relations();
 }
 
 window.onload = generate()
@@ -17,16 +32,43 @@ window.onload = generate()
 function solve(){
     let input = Number(document.getElementById("input").value);
     let currlist = document.getElementById("list")
-    let list = ["過大" , "過小" , "正確"]
+    let list = [" is too large." , " is too small." , " is the correct answer."]
 
     if (input > answer){
         currlist.innerHTML = input + list[0] + `<br>` + currlist.innerHTML;
+        if (input < Window.relationsMax){
+            Window.relationsMax = input
+        }
     }
     else if (input < answer){
         currlist.innerHTML = input + list[1] + `<br>` + currlist.innerHTML;
-    }
+        if (input > Window.relationsMin){
+            Window.relationsMin = input
+        }
+     }
     else{
-        currlist.innerHTML = input + list[2] + `<br>` + currlist.innerHTML;
+        currlist.innerHTML = `<p class="blue">${input + list[2]}</p>` + `<br>` + currlist.innerHTML;
         generate()
     }
+    relations();
+}
+
+function enterGenerate(){
+    if(event.keyCode == 13) {
+        generate();
+    }
+}
+
+function enterSolve(){
+    if(event.keyCode == 13) {
+        solve();
+    }
+}
+
+function relations(){
+    document.getElementById("relations").innerHTML = `${Window.relationsMin} ~ ${Window.relationsMax}`
+}
+
+function clearInput(){
+    document.getElementById("input").value = ""
 }
